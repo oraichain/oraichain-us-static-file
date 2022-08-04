671853cac3117c75cfc03f4f61ee20d12c21d9ea
ducphamle2 <ducphamle212@gmail.com>
2022-08-04 18:24:48 +0700

    update compose & docs for validator setup

--- fullnode/init.sh
+++ fullnode/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DATA_HOME=${DATA_HOME:-"/root/.evmosd"} 
+DATA_HOME=${DATA_HOME:-"/root/.evmosd"}
 
 if [[ -d "$DATA_HOME/data" ]] 
 then
@@ -13,7 +13,7 @@ else
     sed -i "s/enabled-unsafe-cors *= *.*/enabled-unsafe-cors = true/g" $DATA_HOME/config/app.toml
     sed -i "s/cors_allowed_origins *= *.*/cors_allowed_origins = \[\"*\"\]/g" $DATA_HOME/config/config.toml
     sed -i "1,/\<laddr\>/{s/\<laddr\> *= *.*/laddr = \"tcp:\/\/0.0.0.0:26657\"/g}" $DATA_HOME/config/config.toml # replace exactly the string laddr with\< and \>
-    sed -i "s/persistent_peers *= *.*/persistent_peers = \"4725ef9ef94b4a23fcf99b62b2729ad029545c68@159.203.165.199:26656\"/g" $DATA_HOME/config/config.toml
+    # sed -i "s/persistent_peers *= *.*/persistent_peers = \"4725ef9ef94b4a23fcf99b62b2729ad029545c68@159.203.165.199:26656\"/g" $DATA_HOME/config/config.toml
 fi
 
-cosmovisor start --pruning=default --log_level info --json-rpc.api eth,txpool,personal,net,debug,web3
+cosmovisor start --pruning=default --log_level info --json-rpc.api eth,txpool,personal,net,debug,web3 --p2p.persistent_peers "$PERSISTENT_PEERS"

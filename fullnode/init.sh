f635729f46d0fd21143f0d816c82ab860fa9df5c
Duc Pham Le <44611780+ducphamle2@users.noreply.github.com>
2022-08-04 18:25:33 +0700

    Update/optimize and generalize (#1)

--- fullnode/init.sh
+++ fullnode/init.sh
@@ -1,7 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-evmosd init "$NODE_NAME" --chain-id=balcony_666666-1
+DATA_HOME=${DATA_HOME:-"/root/.evmosd"}
 
-cp -R /root/config/* /root/.evmosd/config/
+if [[ -d "$DATA_HOME/data" ]] 
+then
+echo ''
+else
+    evmosd init "$NODE_NAME" --chain-id=balcony_666666-1 --home $DATA_HOME
 
-evmosd start --pruning=nothing --rpc.unsafe --log_level info --json-rpc.api eth,txpool,personal,net,debug,web3 --moniker "$NODE_NAME"
+    wget https://raw.githubusercontent.com/oraichain/oraichain-us-static-file/master/fullnode/config/genesis.json -O $DATA_HOME/config/genesis.json
+
+    sed -i "s/enabled-unsafe-cors *= *.*/enabled-unsafe-cors = true/g" $DATA_HOME/config/app.toml
+    sed -i "s/cors_allowed_origins *= *.*/cors_allowed_origins = \[\"*\"\]/g" $DATA_HOME/config/config.toml
+    sed -i "1,/\<laddr\>/{s/\<laddr\> *= *.*/laddr = \"tcp:\/\/0.0.0.0:26657\"/g}" $DATA_HOME/config/config.toml # replace exactly the string laddr with\< and \>
+    # sed -i "s/persistent_peers *= *.*/persistent_peers = \"4725ef9ef94b4a23fcf99b62b2729ad029545c68@159.203.165.199:26656\"/g" $DATA_HOME/config/config.toml
+fi
+
+cosmovisor start --pruning=default --log_level info --json-rpc.api eth,txpool,personal,net,debug,web3 --p2p.persistent_peers "$PERSISTENT_PEERS"
--- validator/init.sh
+++ validator/init.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-evmosd init "$NODE_NAME" --chain-id=balcony_666666-1
+DATA_HOME=${DATA_HOME:-"/root/.evmosd"} 
 
-cp -R /root/config/* /root/.evmosd/config/
+if [[ -d "$DATA_HOME/data" ]] 
+then
+echo ''
+else
+    evmosd init "$NODE_NAME" --chain-id=balcony_666666-1 --home $DATA_HOME
 
+    wget https://raw.githubusercontent.com/oraichain/oraichain-us-static-file/master/fullnode/config/genesis.json -O $DATA_HOME/config/genesis.json
+fi
+
+cosmovisor start --pruning=default --log_level info --json-rpc.api eth,txpool,personal,net,debug,web3 --p2p.persistent_peers "$PERSISTENT_PEERS"
